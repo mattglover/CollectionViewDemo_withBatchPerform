30b8533449b753463019a731f3843201862e4157
Matt Glover <matt@duchysoftware.com>
2013-10-07 21:24:05 +0100

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by Matt Glover on 07/10/2013.
+//  Copyright (c) 2013 Duchy Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewDemo
+//
+//  Created by Matt Glover on 07/10/2013.
+//  Copyright (c) 2013 Duchy Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
